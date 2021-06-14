b70f5ceac8995308ca22906278fb305d1366f489
Rik Bähnemann <brik@ethz.ch>
2021-06-14 13:05:46 +0200

    Remove deprecated functions.

--- hypersim_multiview/reprojection.py
+++ hypersim_multiview/reprojection.py
@@ -17,28 +17,6 @@ class Reprojection:
         if self.verbose:
             print('Device: {:s}'.format(dev))
 
-    # Given a pixel in source image coordinates, project it into image coordinates of the target frame.
-    # Input: Pixel in source coordinates (width, height), pixel position map source frame, camera pose target frame
-    def projectPixelFromSourceToTarget(self, px_source, source_position_map, R_CW, C_t_CW):
-        # Get world position of source pixel.
-        px_source_int = np.rint(px_source).astype(int)
-        W_t_WP = source_position_map[px_source_int[1], px_source_int[0]]
-        if self.verbose:
-            print('The source pixel [{:d}, {:d}] corresponds to the point [{:.2f}, {:.2f}, {:.2f}] in world coordinates.'.format(px_source_int[0], px_source_int[1], W_t_WP[0], W_t_WP[1], W_t_WP[2]))
-
-        # Transform into target camera using the target cam
-        return self.transformPointWorldToScreen(R_CW, C_t_CW, W_t_WP)
-
-    def projectPixelFromSourceToTargetInt(self, px_source, source_position_map, R_CW, C_t_CW):
-        px_target = self.projectPixelFromSourceToTarget(px_source, source_position_map, R_CW, C_t_CW)
-        return np.rint(px_target).astype(int)
-
-    # Get the projection matrix that projects a homogeneous point in world coordinates onto screen coordinates.
-    def getProjectionMatrix(self, R_CW, C_t_CW):
-        # C_t_CP = C_t_CW + R_CW.dot(W_t_WP)
-        C_T_CW = np.block([[R_CW, np.asmatrix(C_t_CW).T], [np.zeros((1,3)), np.ones((1,1))]])
-        return self.K.dot(C_T_CW)
-
     def maskFov(self, inliers, px):
         inliers = torch.logical_and(inliers, px[:,:,0].ge(0))
         inliers = torch.logical_and(inliers, px[:,:,1].ge(0))
@@ -113,33 +91,6 @@ class Reprojection:
             inlier_mask[inlier_mask==True] = torch.logical_and(inlier_mask[inlier_mask==True], torch.any(reflectance.ge(reflectance_threshold), dim=1))
 
         return px_source, px_target, inlier_mask
-
-
-    # Transform a 3D point from world coordinates into camera coordinates given the camera pose.
-    # Returns [px_width, px_height, depth]
-    def transformPointWorldToScreen(self, R_CW, C_t_CW, W_t_WP):
-        # Project point onto screen.
-        P = self.getProjectionMatrix(R_CW, C_t_CW)
-        W_t_WP_hom = np.concatenate((W_t_WP, [1]), axis=None)
-        p_screen = np.squeeze(np.array(P.dot(W_t_WP_hom)))
-
-        # Normalize
-        p_screen = p_screen / p_screen[3]
-        # Compute pixel coordinates from relative points around camera center.
-        p_uvd = np.zeros(3)
-        p_uvd[0] = 0.5 * (p_screen[0] + 1) * (self.W - 1)
-        p_uvd[1] = (1 - 0.5 * (p_screen[1] + 1)) * (self.H - 1)
-        p_uvd[2] = (p_screen[2] + 1) / 2.0
-        if self.verbose:
-            print('W_t_WP [{:.2f}, {:.2f}, {:.2f}] converted into image coordinates [{:.2f}, {:.2f}, {:.2f}]'.format(W_t_WP[0], W_t_WP[1], W_t_WP[2], p_uvd[0], p_uvd[1], p_uvd[2]))
-        return p_uvd
-
-    def transformPointWorldToScreenInt(self, R_CW, C_t_CW, W_t_WP):
-        p_uvd = self.transformPointWorldToScreen(R_CW, C_t_CW, W_t_WP)
-        return np.rint(p_uvd).astype(int)
-
-    def isFieldOfView(self, p_screen):
-        return np.all(p_screen >= 0) and p_screen[0] < self.W and p_screen[1] < self.H
 
     # Return camera matrix K in 4x4 to project 3D coordinates in camera frame to image plane.
     # https://github.com/apple/ml-hypersim/blob/7bc2a8a751c0157c1bd956972acbdd2ddf85186c/code/python/tools/scene_generate_images_bounding_box.py#L129-L149
